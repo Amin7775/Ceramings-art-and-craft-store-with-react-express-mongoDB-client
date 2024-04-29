93f6e5b3e5329016c150333343c7f25e9e714060
Amin7775 <amin.dpi24@gmail.com>
2024-04-30 01:38:07 +0600

    Added custom colors on tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-color-1': '#BA9D79',
+        'custom-color-2': '#1B1B1B',
+        'custom-color-3': '#F5F5F5',
+        'custom-color-4': '#F2F2F2',
+      }
+    }
   },
   plugins: [
     require('daisyui'),
