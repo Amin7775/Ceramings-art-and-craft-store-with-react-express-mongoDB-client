9d147254c4243c470ede530ad43a4e190a72e025
Amin7775 <amin.dpi24@gmail.com>
2024-04-30 19:56:11 +0600

    added my list cards

--- src/pages/myList/MyList.jsx
+++ src/pages/myList/MyList.jsx
@@ -1,8 +1,39 @@
+import { useContext, useEffect, useState } from "react";
+import CommonTitle from "../../components/commonTitle/CommonTitle";
+import { AuthContext } from "../../provider/AuthProvider";
+import MyListCard from "./MyListCard";
 
 const MyList = () => {
+    const {user} = useContext(AuthContext)
+    const userEmail = user.email
+    console.log(userEmail)
+
+    const [savedData,setSavedData] = useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/allitems')
+        .then(res => res.json())
+        .then(data => setSavedData(data))
+    },[])
+
+    const filterEmail = savedData?.filter(singleData => singleData.User_Email == userEmail)
+    console.log(filterEmail)
+
     return (
-        <div>
-            this is my list
+        <div className="min-h-screen container mx-auto py-14">
+            <CommonTitle text={"My Lists"}></CommonTitle>
+
+            {filterEmail.length == 0 ? 
+            <div className="h-full w-full flex items-center justify-center">
+                <p>You have not added any items yet!</p>
+            </div>
+            :
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+                {
+                    filterEmail?.map(item => <MyListCard key={item._id} item={item}></MyListCard>)
+                }
+            </div>    
+        }
         </div>
     );
 };
---
+++ src/pages/myList/MyListCard.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+
+const MyListCard = ({item}) => {
+
+    const {_id, item_Name, image, subcategory_Name, price, short_Description, rating, customization, processing_Time, stockStatus, User_Email, User_Name
+    } = item;
+
+    return (
+        <div className="card  bg-custom-color-3 drop-shadow-lg">
+        <figure className="px-10 pt-10">
+          <img
+            src={image}
+            alt="Shoes"
+            className="rounded-xl object-cover"
+          />
+        </figure>
+        <div className="card-body items-start text-center">
+          <h2 className="card-title">{item_Name}</h2>
+          <p className="text-lg mt-2"><span className="font-medium">Category : </span> {subcategory_Name}</p>
+          <p className="text-lg "><span className="font-medium">Rating : </span> {rating} out of 5</p>
+          <p className="text-lg "><span className="font-medium">Customization : </span> {customization.toUpperCase()}</p>
+          <p className="text-lg "><span className="font-medium">Stock Status : </span> {stockStatus}</p>
+          <p className="text-lg "><span className="font-medium">Price : </span> {price} taka</p>
+          <div className="w-full">
+              <Link to={`/allitems/${_id}`}>
+            <button className="btn w-full bg-custom-color-2 text-white mt-2 text-lg hover:bg-black/[.8]">Update</button>
+              </Link>
+              <Link to={`/allitems/${_id}`}>
+            <button className="btn w-full bg-red-400 text-white mt-2 text-lg hover:bg-red-400/[.9]">Delete</button>
+              </Link>
+          
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+export default MyListCard;
